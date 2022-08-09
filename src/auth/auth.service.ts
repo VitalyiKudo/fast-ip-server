4a58c550c411db4b5cf741bd211edb6dd4bccfa6
Vitalyi <vitalyipasechnyi@gmail.com>
2022-08-09 13:02:27 +0300

    created the basic token logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/dto/user.servise';
 
 @Injectable()

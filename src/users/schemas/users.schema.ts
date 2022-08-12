01ef81cab34c0f12fbc83c2d190d512c41fd33ad
Vitalyi <vitalyipasechnyi@gmail.com>
2022-08-12 19:51:56 +0300

    add unique

--- src/users/schemas/users.schema.ts
+++ src/users/schemas/users.schema.ts
@@ -2,13 +2,13 @@ import { Column, HasMany, Model, Table } from 'sequelize-typescript';
 import { Views } from 'src/views/schemas/views.schema';
 @Table({ tableName: 'users' })
 export class Users extends Model {
-  @Column({ allowNull: false })
+  @Column({ allowNull: false, unique: true })
   username: string;
 
   @Column
   password: string;
 
-  @Column({ allowNull: false })
+  @Column({ allowNull: false, unique: true })
   key: string;
 
   @Column({ unique: true })

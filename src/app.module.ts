76da0c692206ff2ba30d34c3b3e7c4cd2c4bb686
Vitalyi <vitalyipasechnyi@gmail.com>
2022-08-23 16:08:15 +0300

    add herokue DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,13 @@ import { AppController } from './app.controller';
 @Module({
   imports: [
     SequelizeModule.forRoot({
-      dialect: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'for_views',
+      dialect: 'postgres',
+      uri: process.env.DATABASE_URI,
+      database: 'dtn93i68rsu4h',
+      username: 'yrorrpnitumbpk',
+      password:
+        '7e6075bb13758ac4342d07607d0b71484bc610b1eef7e5ea37ef986d353f1083',
+      synchronize: true,
       autoLoadModels: true,
     }),
     ViewsModule,
